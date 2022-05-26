3f5a5e401e06a06eed432d878b85a329970ab6dd
Michael B. Stearns <michaelbstearns@gmail.com>
2022-05-26 16:14:38 -0700

    GET / now renders a dynamic homepage users can use to login to Auth0 and view their JWT id_token.

--- trucksanditems/__init__.py
+++ trucksanditems/__init__.py
@@ -1,10 +1,20 @@
-from flask import Flask
+import json
 
-from trucksanditems import config
+from flask import Flask, render_template, session
+
+from trucksanditems import auth, config
 
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig())
 
+auth.register_to_auth0(app)
+
+app.register_blueprint(auth.bp)
+
 @app.route("/")
 def home():
-    return "You're home!"
+    return render_template(
+        "home.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4)
+    )
