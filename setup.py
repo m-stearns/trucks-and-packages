fb34955e4fb66517939e9c8610863e0f3f2fd63b
Michael B. Stearns <michaelbstearns@gmail.com>
2022-05-26 11:20:33 -0700

    Initial setup for application.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from pkg_resources import find_distributions
+from setuptools import setup, find_packages
+
+setup(
+    name="trucksanditems",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+    ]
+)
