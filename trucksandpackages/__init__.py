76127a0b1560c66ae735837adb7d3fb7926aaeef
Michael B. Stearns <michaelbstearns@gmail.com>
2022-05-28 22:17:18 -0700

    Home page now only renders a user's ID token (Bearer token) and their unique ID.

--- trucksandpackages/__init__.py
+++ trucksandpackages/__init__.py
@@ -13,8 +13,9 @@ app.register_blueprint(auth.bp)
 
 @app.route("/")
 def home():
+    token = session.get("user")
     return render_template(
         "home.html",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4)
+        session=token,
+        unique_id=token["userinfo"]["sub"]
     )
