96af3bf4ca668a7f5639495d0902ab1b935afcbb
Michael B. Stearns <michaelbstearns@gmail.com>
2022-05-31 11:51:13 -0700

    Implemented get_all_truck_managers() and create_truck_manager() service to handle fetches and persistence of User model.

--- trucksandpackages/services/services.py
+++ trucksandpackages/services/services.py
@@ -1,5 +1,21 @@
+from typing import List
+
 from trucksandpackages.services.unit_of_work import DatastoreUnitOfWork
 from trucksandpackages.domain import model
+
+def create_truck_manager(auth_id: str, unit_of_work: DatastoreUnitOfWork):
+    with unit_of_work:
+        new_user = model.User(auth_id=auth_id)
+        unit_of_work.users.add(new_user)
+        unit_of_work.commit()
+        user_id = unit_of_work.users.id_of_added_entity
+        return user_id
+
+
+def get_all_truck_managers(unit_of_work: DatastoreUnitOfWork) -> List[model.User]:
+    with unit_of_work:
+        truck_managers = unit_of_work.users.get_list()
+        return truck_managers
 
 def create_truck(
     type: str,
@@ -17,7 +33,7 @@ def create_truck(
         truck_id = unit_of_work.trucks.id_of_added_entity
         return truck_id
 
-def get_truck(truck_id: str, unit_of_work: DatastoreUnitOfWork):
+def get_truck(truck_id: str, unit_of_work: DatastoreUnitOfWork) -> model.Truck:
     with unit_of_work:
         truck = unit_of_work.trucks.get(truck_id)
         if truck:
