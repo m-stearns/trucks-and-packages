5368aaa803e880331aa676695433514894483edb
Michael B. Stearns <michaelbstearns@gmail.com>
2022-05-30 21:43:11 -0700

    GET /trucks/:truck_id implemented.

--- trucksandpackages/trucks.py
+++ trucksandpackages/trucks.py
@@ -1,8 +1,10 @@
+from re import T
+from typing import List, Set
 from flask import Blueprint, jsonify, make_response, request
 
-from trucksandpackages import auth, exceptions
+from trucksandpackages import auth, common, exceptions
 from trucksandpackages.services import services, unit_of_work
-from trucksandpackages import common
+from trucksandpackages.domain import model
 
 bp = Blueprint("trucks", __name__, url_prefix="/trucks")
 
@@ -13,6 +15,26 @@ def has_required_values_for_create_truck(json_data: dict) -> bool:
         if value not in json_data:
             return False
     return True
+
+def truck_to_dict(truck: model.Truck, self_link: str, packages_dict: List) -> dict:
+    return {
+        "id": truck.truck_id,
+        "type": truck.truck_type,
+        "length": truck.truck_length,
+        "axles": truck.axles,
+        "packages": packages_dict,
+        "owner": truck.owner,
+        "self": self_link
+    }
+
+def create_list_of_package_dict(packages: Set[model.Package], host_url: str) -> List:
+    return [package_to_dict(package, host_url) for package in packages]
+
+def package_to_dict(package: model.Package, host_url: str):
+    return {
+        "id": package.package_id,
+        "self": f"{host_url}/{package.package_id}"
+    }
 
 @bp.route("", methods=["POST"])
 def create_truck():
@@ -38,8 +60,8 @@ def create_truck():
         if not has_required_values_for_create_truck(json_data):
             response_400_error = make_response(
                 jsonify({
-                "Error": "The request object is missing at least one of the \
-                    required attributes"
+                    "Error": "The request object is missing at least one of the \
+                        required attributes"
                 })
             )
             response_400_error.status_code = 400
@@ -65,3 +87,45 @@ def create_truck():
         )
         res.status_code = 201
         return res
+
+@bp.route("/<truck_id>", methods=["GET"])
+def get_truck(truck_id: str):
+    if request.method == "GET":
+        try:
+            payload = auth.verify_jwt(request)
+        except (exceptions.NoAuthHeaderError, exceptions.InvalidHeaderError) as e:
+            response_401_error = make_response(e.error)
+            response_401_error.status_code = e.status_code
+            return response_401_error
+
+        response_406_error = common.check_for_accept_error_406(
+            request, ["application/json"]
+        )
+        if response_406_error:
+            return response_406_error
+
+        owner = payload["sub"]
+        truck = services.get_truck(
+            truck_id, owner, unit_of_work.DatastoreUnitOfWork()
+        )
+        if not truck:
+            response_400_error = make_response(
+                jsonify({
+                    "Error": "No truck with this truck_id exists"
+                })
+            )
+            response_400_error.status_code = 400
+            return response_400_error
+            
+        elif truck.owner != owner:
+            return "Bad stuff bro", 400
+        else:
+            response_200 = jsonify(
+                truck_to_dict(
+                    truck,
+                    f"{request.base_url}",
+                    create_list_of_package_dict(truck.packages, f"{request.host_url}packages")
+                )
+            )
+            response_200.status_code = 200
+            return response_200
