86e8911d41f8b4b2c9313c7ab199ef03dfd8d9ee
Michael B. Stearns <michaelbstearns@gmail.com>
2022-06-01 15:58:03 -0700

    Fixed bugs in 415 and 406 error handlers.

--- trucksandpackages/common.py
+++ trucksandpackages/common.py
@@ -3,16 +3,19 @@ from typing import List
 from flask import request, Response, make_response
 
 def check_for_content_type_error_415(req: request) -> Response:
-    if req.headers["Content-Type"] != "application/json":
+    if "Content-Type" not in req.headers or \
+        req.headers["Content-Type"] != "application/json":
         error_res = make_response("Unsupported Media Type")
         error_res.status_code = 415
         return error_res
     else:
         return None
 
-def check_for_accept_error_406(req: request, acceptable_MIME_types: List[str]) -> Response:
+def check_for_accept_error_406(
+    req: request, acceptable_MIME_types: List[str]
+) -> Response:
     for mime_type in acceptable_MIME_types:
-        if mime_type == req.headers["Accept"]:
+        if "Accept" in req.headers and mime_type == req.headers["Accept"]:
             return None
     error_res = make_response("Not Acceptable")
     error_res.status_code = 406
